d6ad3d243c0ba409f339a53db49040f998b09815
PraneethSai <36663366+Praneethsai007@users.noreply.github.com>
2019-11-24 22:51:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+       name=='Tap-search'
+       version='1.0'
+       description='Testing Texract application'
+       author='PraneethSaiMadireddi'
+       author_email="praneeths.madireddi@st.niituniversity.in"
+       packages=["Tap-search"]
+       install_requirements=['Texract']
